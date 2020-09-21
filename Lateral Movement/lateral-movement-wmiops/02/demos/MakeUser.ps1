a61f7a06c84e769fc780f8d616b2b04d7e507e0d
um-bra <70138094+um-bra@users.noreply.github.com>
2020-09-21 18:24:02 +0100

    Add files via upload

---
+++ Lateral Movement/lateral-movement-wmiops/02/demos/MakeUser.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module ActiveDirectory
+New-ADUser -Name "Hacked" -GivenName "Hacked" -Surname "Globomantics" -SamAccountName "hacked" `
+-UserPrincipalName "hacked@globomantics.test" -AccountPassword (ConvertTo-SecureString `
+"Password12!@" -AsPlainText -Force) -Enabled $true
